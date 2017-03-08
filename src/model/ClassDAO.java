1249b8029cdf978ac5ea10a25e6567c7809cef96
Paolo Dávila <pdavila@iesebre.com>
2017-03-08 17:23:02 +0100

    Added ClassDAO.java generic

---
+++ src/model/ClassDAO.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateUtil;
+
+/**
+ *
+ * @author pdavila
+ */
+public class ClassDAO<T> {
+    
+    private Session session;
+    private Transaction tx;
+    
+    private Class p;
+    
+    public ClassDAO(Class<T> p) {
+        this.p = p;
+    }
+
+    public long store(T object) throws HibernateException {
+        long id = 0;
+        
+        try {
+            initOperation();
+            id = Long.parseLong(String.valueOf(session.save(object)));
+            tx.commit();
+        } catch (HibernateException he) {
+            tryException(he);
+            throw he;
+        } finally {
+            session.close();
+        }
+        return id;
+    }
+    
+    public void update(T object) throws HibernateException {
+        try {
+            initOperation();
+            session.update(object);
+            tx.commit();
+        } catch (HibernateException he) {
+            tryException(he);
+            throw he;
+        } finally {
+            session.close();
+        }
+    }
+    
+    public void destroy(T object) throws HibernateException {
+        try {
+            initOperation();
+            session.delete(object);
+            tx.commit();
+        } catch (HibernateException he) {
+            tryException(he);
+            throw he;
+        } finally {
+            session.close();
+        }
+    }
+    
+    public T obtain(int object_id) throws HibernateException {
+        T object = null;
+        try {
+            initOperation();
+            object = (T) session.get(p, object_id);
+        } finally {
+            session.close();
+        }
+        return object;
+    }
+    
+    public List<T> obtainList() throws HibernateException {
+        ArrayList<T> list = null;
+        try {
+            initOperation();
+            list = (ArrayList) session.createQuery("from " + p.getSimpleName()).list();
+        } finally {
+            session.close();
+        }
+        return list;
+    }
+    
+    public void initOperation() throws HibernateException {
+        session = HibernateUtil.getSessionFactory().openSession();
+        tx = session.beginTransaction();
+    }
+    
+    private void tryException(HibernateException he) throws HibernateException {
+        tx.rollback();
+        throw new HibernateException("Error a la capa d'accés a dades", he);
+    }
+}
